345d16ad0def47a8c09fb5842226eecc9150f775
Larissanazario <92223222+Larissanazario@users.noreply.github.com>
2022-04-02 01:57:03 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplayspeed: 2000,
+});
+
+
+$('.slider-convenio').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
